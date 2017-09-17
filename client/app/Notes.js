f8d54dcb23b0a19c870a4803030048830501c690
Paul Hanna <paulhanna1933@gmail.com>
2017-09-17 14:50:03 +0300

    add styles to notepad

--- client/app/Notes.js
+++ client/app/Notes.js
@@ -3,24 +3,43 @@ import React, {Component} from 'react';
 export default class Notes extends Component{
   render(){
     return (
-      <div className='components'>
+      <div className='videoNotes'>
             <div className="notes-col">
-              <h3>Notes</h3>
-              <textarea name="notes" id="textarea" cols="30" rows="10" placeholder="Wanna jot down anything?"></textarea>
-              <h4>here is some dummy notes
-                <button id="edit">Edit</button>
-                <button id="delete">delete</button>
-              </h4>
-              <h4>here is some dummy notes
-                <button id="edit">Edit</button>
-                <button id="delete">delete</button>
-              </h4>
-              <h4>here is some dummy notes
-                <button id="edit">Edit</button>
-                <button id="delete">delete</button>
-              </h4>
+              <h3>Notebook</h3>
+              <textarea name="notes" id="textarea" cols="50" rows="10" placeholder="Type your note here and
+press “enter” to submit..."></textarea>
+
+<div className="leftNotes">
+   <div className="eachNotes">
+      <h4>here is some dummy notes</h4>
+      <p>00:44</p>
+       <button id="edit">Edit</button>
+       <button id="delete">delete</button>
+   </div>
+
+   <div className="eachNotes">
+      <h4>here is some dummy notes</h4>
+      <p>00:44</p>
+       <button id="edit">Edit</button>
+       <button id="delete">delete</button>
+   </div>
+
+   <div className="eachNotes">
+      <h4>here is some dummy notes</h4>
+      <p>00:44</p>
+       <button id="edit">Edit</button>
+       <button id="delete">delete</button>
+   </div>
+
+</div>
+
+
+
+
+              </div>
+
             </div>
-      </div>
+
     )
   }
 }
